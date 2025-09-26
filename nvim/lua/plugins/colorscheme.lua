042e3f72da1a1af6bd63095d33fd8e595d6434e8
Ciaran Sanders <ciaransanders@gmail.com>
2025-09-26 14:35:04 -0700

    Better color styling for neovim's statusline (lualine plugin)

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,12 +3,17 @@ return {
     "ellisonleao/gruvbox.nvim",
     opts = {
       transparent_mode = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
       overrides = {
         TreesitterContextBottom = { underline = true, sp = "#665c54" },
         TreesitterContextLineNumberBottom = { underline = true, sp = "#665c54" },
         IlluminatedWordText = { bg = "#665c54", underline = false },
         IlluminatedWordRead = { bg = "#665c54", underline = false },
         IlluminatedWordWrite = { bg = "#665c54", underline = false },
+        StatusLine = { bg = "#3C3836" },
       },
     },
   },
