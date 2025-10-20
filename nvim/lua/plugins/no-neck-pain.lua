396323b4ebc24aabde6b4fbe2acb4774374df2cc
Ciaran Sanders <ciaransanders@gmail.com>
2025-10-20 15:22:53 -0700

    No neck pain increased width

--- nvim/lua/plugins/no-neck-pain.lua
+++ nvim/lua/plugins/no-neck-pain.lua
@@ -2,7 +2,7 @@ return {
   {
     "shortcuts/no-neck-pain.nvim",
     opts = {
-      width = 130,
+      width = 140,
       autocmds = {
         enableOnVimEnter = true,
         skipEnteringNoNeckPainBuffer = true,
