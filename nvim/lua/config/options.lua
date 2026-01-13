b28cb5d846c013c6f7fde76fff77c3d620e8f5af
Ciaran Sanders <ciaransanders@gmail.com>
2026-01-13 12:45:55 -0800

    Disabled auto-inserting comment characters when entering a newline in neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,10 @@
 -- Add any additional options here
 
 vim.opt.path:append("**") -- Search recursively in subdirectories
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
+  desc = "Disable New Line Comment",
+})
