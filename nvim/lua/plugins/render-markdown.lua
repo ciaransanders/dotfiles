cef20c49872411ab6674662665510f3cc20f7f8b
Ciaran Sanders <ciaransanders@gmail.com>
2026-01-29 15:27:29 -0800

    Config file for render-markdown.nvim plugin

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        sign = true,
+        position = "inline",
+        icons = {
+          "󰼏 ",
+          "󰼐 ",
+          "󰼑 ",
+          "󰼒 ",
+          "󰼓 ",
+          "󰼔 ",
+        },
+        width = "block",
+        left_pad = 1,
+        right_pad = 1,
+      },
+      checkbox = {
+        enabled = true,
+      },
+    },
+  },
+}
