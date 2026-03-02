85add77099e466a08a2244e5d1cbeedea3727775
Ciaran Sanders <ciaransanders@gmail.com>
2026-03-02 12:53:32 -0800

    Configured codecompanion-history.nvim

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -5,8 +5,18 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "ravitemer/codecompanion-history.nvim",
     },
     opts = {
+      extensions = {
+        history = {
+          enabled = true,
+          opts = {
+            expiration_days = 21,
+            picker = "snacks",
+          },
+        },
+      },
       interactions = {
         chat = {
           opts = {
