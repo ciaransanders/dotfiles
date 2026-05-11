1c620c15ea1388a13ecc3d4f067011f96ae765ea
Ciaran Sanders <ciaransanders@gmail.com>
2026-05-11 09:23:20 -0700

    Removed delay from which-key.nvim

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,6 +1,7 @@
 return {
   "folke/which-key.nvim",
   opts = {
+    delay = 0,
     spec = {
       { "<leader>m", group = "map", icon = { color = "cyan", icon = " " } },
       { "<leader>a", group = "ai", icon = { color = "cyan", icon = "󰚩 " } },
