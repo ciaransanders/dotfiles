99e170861acb1b223321202bc430255e56b98105
Ciaran Sanders <ciaransanders@gmail.com>
2026-08-11 10:12:15 -0700

    Center screen after search navigation in neovim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ vim.keymap.set("n", "<C-L>", "20zl")
 vim.keymap.set("n", "<C-H>", "20zh")
 vim.keymap.set("v", "<C-L>", "<Cmd>normal! 20zl<CR>20l")
 vim.keymap.set("v", "<C-H>", "<Cmd>normal! 20zh<CR>20h")
+
+-- Center screen aftger using n and N for search navigation
+vim.keymap.set("n", "n", "nzzzv", { silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { silent = true })
